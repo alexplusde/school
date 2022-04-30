5ef4d6ce61e0156507db28b12071a3bb77757b38
Alexander Walther <aw@alexplus.de>
2022-04-30 13:49:59 +0200

    link in redactor editor

--- boot.php
+++ boot.php
@@ -33,3 +33,49 @@ if (rex::isBackend() && rex::isDebugMode()) {
     school::writeModule();
     school::writeTemplate();
 }
+
+
+\rex_extension::register('OUTPUT_FILTER', function (\rex_extension_point $ep) {
+    return preg_replace_callback(
+        '@(rex-(school-team|school-subject|school-projects|school-room|school-course|event-date|event-category))://(\d+)(?:-(\d+))?/?@i',
+        function ($matches) {
+            // table = $matches[1]
+            // id = $matches[3]
+            $url = '';
+            switch ($matches[1]) {
+                case 'rex-school-team':
+                    $person = team::get($matches[3]);
+                    if ($person) {
+                        $url = $person->getUrl();
+                    }
+                    break;
+                case 'rex-event-date':
+                    $event_date = event_date::get($matches[3]);
+                    if ($event_date) {
+                        $url = $event_date->getUrl();
+                    }
+                    break;
+                case 'rex-event-category':
+                    $event_category = event_category::get($matches[3]);
+                    if ($event_category) {
+                        $url = $event_category->getUrl();
+                    }
+                    break;
+                case 'rex-school-course':
+                    $school_course = school_course::get($matches[3]);
+                    if ($school_course) {
+                        $url = $school_course->getUrl();
+                    }
+                    break;
+                case 'rex-school-projects':
+                    $school_project = school_project::get($matches[3]);
+                    if ($school_project) {
+                        $url = $school_project->getUrl();
+                    }
+                    break;
+                }
+            return $url;
+        },
+        $ep->getSubject()
+    );
+}, rex_extension::NORMAL);
