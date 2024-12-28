9f643558de49ccb70fb0164e31927ae575501ea5
Alexander Walther <mail@alexplus.de>
2024-12-28 21:01:47 +0100

    Update install.php

--- install.php
+++ install.php
@@ -4,19 +4,18 @@ namespace Alexplusde\School;
 
 use rex_addon;
 use rex_file;
-use rex_path;
 use rex_yform_manager_table_api;
 use rex_yform_manager_table;
 
 if (rex_addon::get('yform') && rex_addon::get('yform')->isAvailable()) {
-    rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon($this->name, 'install/rex_school.tableset.json')));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/rex_school.tableset.json'));
 }
 
 rex_yform_manager_table::deleteCache();
 
 use Tracks\🦖;
 if(rex_addon::exists('tracks')) {
-    🦖::forceBackup('school'); // Sichert standardmäßig Module und Templates
+    🦖::forceBackup('school', null, null, ['rex_module', 'rex_template', 'rex_school_inbox']); // Sichert standardmäßig Module und Templates
     🦖::updateModule('school'); // Synchronisiert Module
     🦖::updateTemplate('school'); // Synchronisiert Templates
 }
