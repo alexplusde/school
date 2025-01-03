15588336edd1fc4214ed5e9a7a78b722f7dfb6ae
Alexander Walther <mail@alexplus.de>
2025-01-03 21:43:11 +0100

    Template: CSS dynamisieren

--- install/template/school_default.php
+++ install/template/school_default.php
@@ -1,7 +1,9 @@
 <?php
 use Alexplusde\BS5\Fragment;
+use Alexplusde\BS5\Template;
 
 $clang = rex_clang::getCurrent();
+setlocale(LC_ALL, $clang->getValue('locale'), $clang->getCode()); 
 
 $fragment = new Fragment();
 $fragment->setVar('TEMPLATE_KEY', "REX_TEMPLATE_KEY");
@@ -14,11 +16,10 @@ $fragment->setVar('content', $fragment->getVar('content1') . $fragment->getVar('
 <html class="template-REX_TEMPLATE_KEY" lang="<?= $clang->getCode(); ?>">
     <head>
         <link href="<?= speed_up_asset::getUrl("addons/project/css/project.css") ?>" rel="stylesheet" type="text/css">
-        <link href="<?= speed_up_asset::getUrl("addons/school/css/flexslider.css") ?>" rel="stylesheet" type="text/css">
         <link href="<?= speed_up_asset::getUrl("addons/school/css/magnific-popup.css") ?>" rel="stylesheet" type="text/css">
         <link href="<?= speed_up_asset::getUrl("addons/project/fonts/open-sans.css") ?>" rel="stylesheet" type="text/css">
         <link href="<?= speed_up_asset::getUrl("addons/plus_bs5/fonts/bootstrap-icons.css") ?>" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="<?= speed_up_asset::getUrl('addons/plyr/vendor/plyr/dist/plyr.css') ?>">
+<?php Template::showCss() ?>
         <?= $fragment->parse('bs5/template/meta.php'); ?>
     </head>
 
@@ -32,10 +33,9 @@ $fragment->setVar('content', $fragment->getVar('content1') . $fragment->getVar('
         <script src="<?= speed_up_asset::getUrl('addons/project/js/bootstrap.bundle.min.js') ?>"></script>
         <script src="<?= speed_up_asset::getUrl("addons/school/js/jquery-3.7.1.min.js") ?>"></script>
         <script src="<?= speed_up_asset::getUrl("addons/school/js/jquery.magnific-popup.min.js") ?>"></script>
-        <script src="<?= speed_up_asset::getUrl("addons/school/js/jquery.flexslider-min.js") ?>"></script>
-        <script src="<?= speed_up_asset::getUrl("addons/school/js/jquery.tablesorter.combined.min.js") ?>"></script>
-        <script src="<?= speed_up_asset::getUrl('addons/plyr/vendor/plyr/dist/plyr.min.js') ?>"></script>
-        <script src="<?= speed_up_asset::getUrl('addons/plyr/plyr_init.js') ?>"></script>
+        <?php Template::showCss() ?>
+        <!--<script src="<?= speed_up_asset::getUrl('addons/plyr/vendor/plyr/dist/plyr.min.js') ?>"></script>-->
+        <!--<script src="<?= speed_up_asset::getUrl('addons/plyr/plyr_init.js') ?>"></script>-->
         <script src="<?= speed_up_asset::getUrl("addons/project/js/project.js") ?>"></script>
     </body>
 </html>
